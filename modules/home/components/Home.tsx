2adfd3cdb4da06279db7fbc7b22a127149863f85
Satria Bahari <satriaabaharii@gmail.com>
2024-04-20 15:27:44 +0700

    feat: create home feature

---
+++ modules/home/components/Home.tsx
@@ -0,0 +1,13 @@
+import About from "./About";
+import Contacts from "./Contacts";
+import Introduction from "./Introduction";
+
+export default function Home() {
+  return (
+    <>
+      <Introduction />
+      <About />
+      <Contacts />
+    </>
+  );
+}
