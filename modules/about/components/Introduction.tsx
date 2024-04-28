17e2ec63b97e0932f95e823ef516e98cdf3f7e00
Satria Bahari <satriaabaharii@gmail.com>
2024-04-28 13:18:52 +0700

    feat: add about feature

--- modules/about/components/Introduction.tsx
+++ modules/about/components/Introduction.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Introduction() {
   return (
-    <section className="space-y-4">
+    <section className="space-y-4 text-center text-neutral-900 leading-[1.8] lg:px-32 px-0">
       {ABOUT.map((paragraph, index) => (
         <div key={index}>{paragraph}</div>
       ))}
