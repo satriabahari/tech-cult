7948d968c25eaeeeda95d412f7cf4d2dfe9eeb6d
Satria Bahari <satriaabaharii@gmail.com>
2024-04-28 13:05:45 +0700

    style: add scroll smooth class

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en" suppressHydrationWarning={true} className="!scroll-smooth">
       <body className={soraSans.className}>
         <ThemeProviderContext>
           <Layouts>{children}</Layouts>
