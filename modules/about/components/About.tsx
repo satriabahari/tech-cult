dac8e4c74314d59d32bbf397b73b8d26b2df6e92
Satria Bahari <satriaabaharii@gmail.com>
2024-04-28 13:19:01 +0700

    feat: add about feature

--- modules/about/components/About.tsx
+++ modules/about/components/About.tsx
@@ -5,8 +5,8 @@ import Introduction from "./Introduction";
 
 export default function About() {
   return (
-    <Container id="about">
-      <PageHeading title="About" />
+    <Container id="tentang" data-aos="fade-up" className="min-h-screen h-full mb-12">
+      <PageHeading title="Tentang" />
       <Introduction />
     </Container>
   );
