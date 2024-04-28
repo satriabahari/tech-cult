30060dbadb74deaa64e3060d9cd7d9c6d3b8f69f
Satria Bahari <satriaabaharii@gmail.com>
2024-04-28 13:17:58 +0700

    refactor: add AOS

--- common/components/layouts/index.tsx
+++ common/components/layouts/index.tsx
@@ -1,11 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
 import Header from "./header/Header";
 import Footer from "./partials/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layouts({ children }: LayoutProps) {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      delay: 50,
+      once: true,
+      offset: 200
+    });
+  });
   return (
     <div>
       <Header />
