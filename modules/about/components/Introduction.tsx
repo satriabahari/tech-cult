b67ff25e114366eaf3da87623605d2e3a888e85f
Satria Bahari <satriaabaharii@gmail.com>
2024-05-03 02:21:17 +0700

    style: chage some class

--- modules/about/components/Introduction.tsx
+++ modules/about/components/Introduction.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Introduction() {
   return (
-    <section className="space-y-4 text-center text-neutral-900 leading-[1.8] lg:px-32 px-0">
+    <section className="space-y-4 text-center text-neutral-900 dark:text-neutral-400 leading-[1.8] lg:px-32 px-0">
       {ABOUT.map((paragraph, index) => (
         <div key={index}>{paragraph}</div>
       ))}
