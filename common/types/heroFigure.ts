22a0d50f825f81d9871a3ff11bc5979522ec4eb4
Satria Bahari <satriaabaharii@gmail.com>
2024-05-03 02:19:11 +0700

    refactor: add hero figure type

---
+++ common/types/heroFigure.ts
@@ -0,0 +1,10 @@
+export interface HeroFigureProps {
+  name: string;
+  date_of_birth?: string;
+  place_of_birth?: string;
+  date_of_death?: string;
+  place_of_death?: string;
+  description: string;
+  image: string;
+  isShow?: boolean;
+}
