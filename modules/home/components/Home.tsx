5cd0ddcffa127e292f6a5598631f94aff659d76c
Satria Bahari <satriaabaharii@gmail.com>
2024-05-03 02:21:34 +0700

    refactor: change favicon

--- modules/home/components/Home.tsx
+++ modules/home/components/Home.tsx
@@ -1,16 +1,32 @@
 import Container from "@/common/components/elements/Container";
+import useIsIntersectionObserver from "@/common/hooks/useIsIntersectionObserver";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <Container className="flex h-screen flex-col items-center justify-center gap-y-1">
-      <h1 className="text-3xl font-medium uppercase text-neutral-900">
-        Budaya Nusantara
-      </h1>
-      <h1 className="text-3xl font-semibold uppercase text-red-500">Indonesia</h1>
-      <p>
-        <span className="font-semibold">Pulih</span> lebih cepat,{" "}
-        <span className="font-semibold">Bangkit</span> lebih kuat.
-      </p>
+    <Container
+      id="beranda"
+      data-aos="fade-up"
+      className="relative flex h-screen flex-col items-center justify-center gap-y-1"
+    >
+      <Image
+        src={"/images/backgrounds/indonesia.png"}
+        alt="Indonesia"
+        fill={true}
+        className="lg:object-fit object-none py-32 px-4 opacity-15"
+      />
+      <div className="absolute text-center lg:space-y-3 space-y-1">
+        <h1 className="lg:text-5xl text-3xl font-medium uppercase text-neutral-900 dark:text-neutral-50">
+          Budaya Nusantara
+        </h1>
+        <h1 className="lg:text-4xl text-2xl font-semibold uppercase text-red-500">
+          Indonesia
+        </h1>
+        <p className="text-neutral-900 dark:text-neutral-50 lg:text-2xl">
+          <span className="font-semibold">Pulih</span> lebih cepat,{" "}
+          <span className="font-semibold">Bangkit</span> lebih kuat.
+        </p>
+      </div>
     </Container>
   );
 }
